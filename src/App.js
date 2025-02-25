7cd166107d2f1111d3be838f6a7f17f376733271
정현진 <hyeonjin0905@inzent.com>
2025-02-25 17:56:39 +0900

    JSX 문법 익히기

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let div = 'black-nav';
+  let post = '블로그 글 제목';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    {/**
+     * html이 아니라 jsx이다.
+     * .js 파일에서 쓰는 html 대용품
+     */}
+      <div className ={div}>
+        {/**
+         * JSX 문법 1.
+         * class 넣을 땐 className으로 사용한다.
+         */}
+        <h4 style={ {color: 'red', fontSize: '16px'} }>Blog</h4>
+        {/**
+         * JSX 문법 3.
+         * style 넣을 땐 style = { {스타일명 : '값'} }
+         */}
+      </div>
+      {/**
+       * JSX 문법 2.
+       * 변수 넣을 땐(데이터 바인딩) {중괄호}
+       */}
+      <h4>{post}</h4>
     </div>
   );
 }
